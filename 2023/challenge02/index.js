3b305bdb504386565c6840470935b8225ac05e8f
Xavi Alfaro <nxb.developer@gmail.com>
2023-11-10 18:44:50 -0500

    add challenge02

---
+++ 2023/challenge02/index.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+const message = fs.readFileSync('message_02.txt', 'utf-8');
+
+function challengeTwo(string) {
+  let currentValue = 0
+  let output = ''
+  for (let i = 0; i < string.length; i++) {
+    const currentSymbol = string.at(i)
+    if (currentSymbol === '#') {
+      currentValue += 1
+    } else if (currentSymbol === '@') {
+      currentValue -= 1
+    } else if (currentSymbol === '*') {
+      currentValue *= currentValue
+    } else {
+      output += `${currentValue}`
+    }
+  }
+  return output
+}
+
+console.log(challengeTwo(message))
